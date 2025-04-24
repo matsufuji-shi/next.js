480aad4ed47c83c5f93374dd9680927b7736fc92
shiori matsufuji <s.matsufuji@ebacorp.jp>
2025-04-24 12:29:24 +0900

    入力欄変更

--- src/pages/add-product.js
+++ src/pages/add-product.js
@@ -31,7 +31,7 @@ export default function Form(){
         />
         <input
           type="text"
-          placeholder="値段"
+          placeholder="値段を入力"
           value={price}
           onChange={(e) => setPrice(e.target.value)}
         />
